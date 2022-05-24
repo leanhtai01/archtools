9686cc48bbe9893c833cc595d64acf2e6def80e8
leanhtai01 <leanhtai01@gmail.com>
2022-05-24 17:22:12 +0700

    add function make partitions for windows

--- lib/diskutils.py
+++ lib/diskutils.py
@@ -70,6 +70,38 @@ def shrink_partition(device, partnum, space_to_shrink, unit='GiB'):
     ], input=info.encode())
 
 
+def make_partitions_for_windows(device):
+    """make partition for Windows"""
+    esp_partnum = '1'
+    msftres_partnum = '2'
+    msftdata_partnum = '3'
+    winre_partnum = '4'
+
+    wipe_device(device)
+
+    create_partition(device, 'ef00', 'EFI system partition', '+100M')
+    create_partition(device, '0c01', 'Microsoft reserved partition', '+16M')
+    create_partition(device, '0700', 'Basic data partition', '0')
+    shrink_partition(device, msftdata_partnum, '509', 'MiB')
+    create_partition(device, '2700', '', '0')
+
+    partition_prefix = device + 'p' if device.startswith('nvme') else device
+    esp_part_name = partition_prefix + esp_partnum
+    msftres_part_name = partition_prefix + msftres_partnum
+    msftdata_part_name = partition_prefix + msftdata_partnum
+    winre_part_name = partition_prefix + winre_partnum
+
+    wipe_partition(esp_part_name)
+    wipe_partition(msftres_part_name)
+    wipe_partition(msftdata_part_name)
+    wipe_partition(winre_part_name)
+
+    format_fat32(esp_part_name)
+    format_ntfs(msftdata_part_name)
+    format_ntfs(winre_part_name)
+    set_partition_flag_state(device, winre_partnum, 'hidden', 'on')
+
+
 def prepare_unencrypted_layout(
     device, esp_size='+550M', boot_size='+550M', swap_size='+20G'
 ):
