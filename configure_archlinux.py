3d6d926016b977e50d002d7e5a4d9a7b6bf93b27
leanhtai01 <leanhtai01@gmail.com>
2022-08-24 09:24:32 +0700

    install and configure input method based on DE

--- configure_archlinux.py
+++ configure_archlinux.py
@@ -4,7 +4,7 @@ arch_install = ArchInstall('settings.json', live_system=False)
 
 arch_install.execute_method(arch_install.configure_gnome)
 arch_install.execute_method(arch_install.make_gnome_shortcuts)
-arch_install.execute_method(arch_install.configure_ibus_bamboo)
+arch_install.execute_method(arch_install.configure_input_method)
 arch_install.execute_method(arch_install.enable_gnome_appindicator)
 arch_install.execute_method(arch_install.enable_gnome_vitals_extension)
 arch_install.execute_method(arch_install.configure_gedit)
