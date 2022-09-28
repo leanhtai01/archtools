1dfa64b7c00033138b0972f6aef38cd8448e514a
leanhtai01 <leanhtai01@gmail.com>
2022-09-28 08:27:51 +0700

    configure: not install flatpak packages

--- configure_archlinux.py
+++ configure_archlinux.py
@@ -8,7 +8,3 @@ arch_install.execute_method(arch_install.configure_input_method)
 arch_install.execute_method(arch_install.enable_gnome_appindicator)
 arch_install.execute_method(arch_install.enable_gnome_vitals_extension)
 arch_install.execute_method(arch_install.configure_gedit)
-arch_install.execute_method(
-    arch_install.install_flatpak_packages_from_file,
-    'packages_info/arch_linux/flatpak.txt'
-)
